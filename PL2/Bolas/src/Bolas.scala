c18c767804844c00f9894aeacd7d41001afe30bf
javi040898 <57791218+javi040898@users.noreply.github.com>
2020-03-24 13:00:52 +0100

    empezando juego bolas

--- PL2/Bolas/src/Bolas.scala
+++ PL2/Bolas/src/Bolas.scala
@@ -2,10 +2,8 @@
 
 object Bolas {
   
-
-
                       
-  def llenar_tablero(tablero:List[List[Char]],cont:Int):List[List[Char]]={
+  def llenar_tablero_inicial(tablero:List[List[Char]],cont:Int):List[List[Char]]={
     
     val lista = List('A','N','R','V','M','G')
     
@@ -22,17 +20,34 @@ object Bolas {
     else{//Si no se han llenado los 9
       if (tablero(posicion1)(posicion2)=='_'){//Si esa posicion no se ha reemplazado
         val tablero_inicial = tablero(posicion1).updated(posicion2, valor)
-        val tablero_final = tablero.updated(posicion1, tablero_inicial)
-        llenar_tablero(tablero_final,cont+1)//Llamamos a la funcion con contador +1
+        val tablero_inicial_final = tablero.updated(posicion1, tablero_inicial)
+        llenar_tablero_inicial(tablero_inicial_final,cont+1)//Llamamos a la funcion con contador +1
       }
-      else {llenar_tablero(tablero,cont)}//Si la posicion esta reemplazada volvemos otra vez
+      else {llenar_tablero_inicial(tablero,cont)}//Si la posicion esta reemplazada volvemos otra vez
       
    }
         
   }
+  
+  def mover_bola(tablero:List[List[Char]]):List[List[Char]]={
+     
+     print("Introduzca la fila de la bola que quiere utilizar(0-8)=: ")
+     val x = scala.io.StdIn.readInt()
+     print("Introduzca la columna de la bola que quiere utilizar(0-8): ")
+     val y = scala.io.StdIn.readInt()
+     val bola = tablero(x)(y)
+     if(bola!='_'){
+       //Aqui esta la chicha
+     }
+     else{
+       println("Posicion vacia, vuelva a introducir ")
+       mover_bola(tablero)
+     }
+     return tablero
+  }
 
-  def mostrar_tablero(tablero:List[List[Char]]){//Mostrar el tablero bien
-    val tablero_final=llenar_tablero(tablero,0)
+  def mostrar_tablero(tablero:List[List[Char]]):List[List[Char]]={//Mostrar el tablero bien
+    val tablero_final=llenar_tablero_inicial(tablero,0)
     println(tablero_final(0))
     println(tablero_final(1))
     println(tablero_final(2))
@@ -42,6 +57,7 @@ object Bolas {
     println(tablero_final(6))
     println(tablero_final(7))
     println(tablero_final(8))
+    return tablero_final
   }
                       
   def main(args: Array[String]){
@@ -56,7 +72,11 @@ object Bolas {
                       List('_','_','_','_','_','_','_','_','_'))
 
     
-    mostrar_tablero(tablero_vacio)
+    mover_bola(mostrar_tablero(tablero_vacio))
+
+   
+
+
     
   }
 }
