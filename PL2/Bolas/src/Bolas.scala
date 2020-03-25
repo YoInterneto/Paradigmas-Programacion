51e8c3a0a066f96bb2ee29d79d4b429cf8abb292
javi040898 <57791218+javi040898@users.noreply.github.com>
2020-03-25 14:53:39 +0100

    poquito a poquito
    suave suave suavesito

--- PL2/Bolas/src/Bolas.scala
+++ PL2/Bolas/src/Bolas.scala
@@ -29,50 +29,156 @@ object Bolas {
         
   }
   
-  def mover_bola(tablero:List[List[Char]]):List[List[Char]]={
+  def comprobar_limite(x:Int,y:Int):Boolean={
+    
+    if(x<=8 && x>=0 && y>=0 && y<=8){
+      return true
+    }
+    else{
+      println("Se ha pasado de los limites del tablero")
+      return false
+    }
+  }
+  
+  def escoger_bola(tablero:List[List[Char]]){
      
-     print("Introduzca la fila de la bola que quiere utilizar(0-8)=: ")
-     val x = scala.io.StdIn.readInt()
-     print("Introduzca la columna de la bola que quiere utilizar(0-8): ")
-     val y = scala.io.StdIn.readInt()
-     val bola = tablero(x)(y)
-     if(bola!='_'){
-       //Aqui esta la chicha
+     if(final_partida(tablero, 0, 0)){
+       println("Tablero completo, partida finalizada")
      }
      else{
-       println("Posicion vacia, vuelva a introducir ")
-       mover_bola(tablero)
+       print("Introduzca la fila de la bola que quiere utilizar(0-8): ")
+       val x = scala.io.StdIn.readInt()
+       print("Introduzca la columna de la bola que quiere utilizar(0-8): ")
+       val y = scala.io.StdIn.readInt()
+       if(comprobar_limite(x,y)){
+       val bola = tablero(x)(y)
+         if(bola!='_'){
+           println("Bola escogida:"+ bola)
+           mover_bola(tablero,bola,x,y)
+         }
+         else{
+           println("Posicion vacia, vuelva a introducir ")
+           escoger_bola(tablero)
+         }
+       }
+       else{
+         escoger_bola(tablero)
+       }
+     
+     
+    
      }
-     return tablero
+     
+  }
+  
+  def mover_bola(tablero:List[List[Char]],bola:Char,x:Int,y:Int){
+    
+    print("Introduzca la fila donde la quiere introducir(0-8): ")
+    val x2 = scala.io.StdIn.readInt()
+    print("Introduzca la columna donde la quiere introducir(0-8): ")
+    val y2 = scala.io.StdIn.readInt()
+    if(comprobar_limite(x2, y2)){
+      val hueco = tablero(x2)(y2)
+      if(hueco=='_'){
+        val tablero_inicial = tablero(x2).updated(y2, bola)
+        val tablero_sin_hueco = tablero.updated(x2, tablero_inicial)
+        val tablero_hueco = tablero_sin_hueco(x).updated(y, '_')
+        val tablero_con_hueco = tablero_sin_hueco.updated(x, tablero_hueco)
+        
+        //escoger_bola(tablero_con_hueco)
+        val tablero_sig_turno = rellenar_turno(tablero_con_hueco, 0)
+        mostrar_tablero(tablero_sig_turno)
+        escoger_bola(tablero_sig_turno)
+         
+      }
+      else{
+        println("Hueco ocupado, no puede introducir la bola en este hueco")
+        mover_bola(tablero,bola,x,y)
+      }
+    }
+    else{
+      mover_bola(tablero,bola,x,y)
+    }
+    
+    
+  }
+  
+  def rellenar_turno(tablero:List[List[Char]],cont:Int):List[List[Char]]={//Para rellenar las 3 en cada turno
+    
+    val lista = List('A','N','R','V','M','G')
+    val r = scala.util.Random
+    val color = r.nextInt(6)
+    val posicion1 = r.nextInt(9)
+    val posicion2 = r.nextInt(9)
+    val valor = lista(color)
+    
+    if(cont==3){
+      return tablero
+    }
+    else{
+      
+      if(tablero(posicion1)(posicion2)=='_'){//Si esa posicion no se ha reemplazado
+        val tablero_inicial = tablero(posicion1).updated(posicion2, valor)
+        val tablero_inicial_final = tablero.updated(posicion1, tablero_inicial)
+        rellenar_turno(tablero_inicial_final,cont+1)//Llamamos a la funcion con contador +1
+      }
+      else {
+        rellenar_turno(tablero,cont)
+      }//Si la posicion esta reemplazada volvemos otra vez
+        
+    }
+    
+  }
+  //Falta controlar cuando al final quedan 2 o 1 hueco poque se raya al reemplazar las 3 
+  def final_partida(tablero:List[List[Char]],fila:Int,columna:Int):Boolean={
+    if(fila==9){
+      return true
+    }
+    else{
+      if(columna==9){
+        final_partida(tablero,fila+1,0)
+      }
+      else{
+        if(tablero(fila)(columna)=='_'){
+          return false
+      }
+        else{
+          final_partida(tablero,fila,columna+1)
+          
+        }
+      }
+      
+    }
   }
 
   def mostrar_tablero(tablero:List[List[Char]]):List[List[Char]]={//Mostrar el tablero bien
-    val tablero_final=llenar_tablero_inicial(tablero,0)
-    println(tablero_final(0))
-    println(tablero_final(1))
-    println(tablero_final(2))
-    println(tablero_final(3))
-    println(tablero_final(4))
-    println(tablero_final(5))
-    println(tablero_final(6))
-    println(tablero_final(7))
-    println(tablero_final(8))
-    return tablero_final
+    
+    println(tablero(0))
+    println(tablero(1))
+    println(tablero(2))
+    println(tablero(3))
+    println(tablero(4))
+    println(tablero(5))
+    println(tablero(6))
+    println(tablero(7))
+    println(tablero(8))
+    return tablero
   }
                       
   def main(args: Array[String]){
-      val tablero_vacio = List(List('_','_','_','_','_','_','_','_','_'),
-                      List('_','_','_','_','_','_','_','_','_'),
-                      List('_','_','_','_','_','_','_','_','_'),
-                      List('_','_','_','_','_','_','_','_','_'),
-                      List('_','_','_','_','_','_','_','_','_'),
-                      List('_','_','_','_','_','_','_','_','_'),
-                      List('_','_','_','_','_','_','_','_','_'),
-                      List('_','_','_','_','_','_','_','_','_'),
-                      List('_','_','_','_','_','_','_','_','_'))
+      val tablero_vacio = List(List('A','A','A','A','A','A','A','A','A'),
+                      List('N','G','R','A','A','A','A','A','A'),
+                      List('_','_','_','_','G','G','A','G','G'),
+                      List('A','A','A','A','_','A','A','G','A'),
+                      List('A','_','G','G','G','G','A','_','A'),
+                      List('A','_','G','G','G','_','A','_','A'),
+                      List('A','_','G','_','G','_','A','A','A'),
+                      List('A','_','A','A','A','_','A','A','A'),
+                      List('A','A','G','G','G','G','A','A','A'))
 
-    
-    mover_bola(mostrar_tablero(tablero_vacio))
+    val tablero_inicial=llenar_tablero_inicial(tablero_vacio,0)
+    mostrar_tablero(tablero_inicial)
+    escoger_bola(tablero_inicial)
 
    
 
